400556c99cef3bfb738ad4791b11b64b8571b5dd
a18marcastru <a18marcastru@inspedralbes.cat>
2025-01-30 18:12:13 +0100

    Funciona

--- M06/Prova_validacio_2/db.js
+++ M06/Prova_validacio_2/db.js
@@ -1,34 +1,38 @@
-import mongo from "mongoose";
+import { MongoClient } from "mongodb";
+
+var database;
+var client;
 
 export async function connectarBD(URI, DB_NAME) {
+    const mongoUrl = URI;
+    client = new MongoClient(mongoUrl);
+
     try {
-        const client = await mongo.connect(URI + DB_NAME);
-        return client.db();
+        database = client.db(DB_NAME);
+        console.log('Connexió establerta correctament!');
     } catch (err) {
         console.log("Error de conexió", err);
     }
 }
 
-
-export async function inserirAlumnes(URI, DB_NAME, alumnesPerInserir) {
+export async function inserirAlumnes(alumnesPerInserir) {
     try {
-        const database = await connectarBD(URI, DB_NAME);
-        const result = await database.collection('alunmnes').insertMany(alumnesPerInserir)
-        return result;
+        const result = await database.collection('alumnes').insertMany(alumnesPerInserir);
+        return result.insertedCount;
     } catch(err) {
         console.log(err);
     }
 }
 
-
-/*
 export async function obtenirAlumnes() {
-    return await db.collection('alumnesDB').find();
+    try {
+        const result = await database.collection('alumnes').find().toArray();
+        return result;
+    } catch (err) {
+        console.log(err);
+    }
 }
-*/
 
 export async function tancarBD() {
-    mongo.connection.close()
-    .then(() => console.log('Conexión cerrada'))
-    .catch(err => console.error(err));
+    await client.close()
 }
