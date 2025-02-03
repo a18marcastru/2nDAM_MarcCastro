349af9f5934eb9cba250330d13c95f3e5640fd41
a18marcastru <a18marcastru@inspedralbes.cat>
2025-02-03 18:55:13 +0100

    Exercici

---
+++ M09/UF3/Exercicis_TR2_PV_2/Exercici1/web/vuetify-project/src/components/Services.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-app>
+        <v-container>
+            <v-row>
+                <h2>Hola gente</h2>
+            </v-row>
+        </v-container>
+    </v-app>
+</template>
+
+<script setup>
+</script>
