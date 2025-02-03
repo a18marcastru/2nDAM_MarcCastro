589c72f913dd5b3745d38bcff881d62c54bee677
a18marcastru <a18marcastru@inspedralbes.cat>
2025-02-03 23:29:50 +0100

    Scripts

--- M09/UF3/Exercicis_TR2_PV_2/Exercici1/back/server2.js
+++ M09/UF3/Exercicis_TR2_PV_2/Exercici1/back/server2.js
@@ -1,1 +1,28 @@
 "use strict";
+const express = require('express');
+const app = express();
+const { spawn } = require('child_process');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+const fs = require('fs');
+const path = require('path');
+
+const server = createServer(app);
+const io = new Server(server);
+const port = 3000;
+
+io.on('connection', (socket) => {
+    console.log('A user connected', socket.id);
+
+    io.on("service2", ({ service, execute }) => {
+        
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected', socket.id);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server 1 listening port ${port}`);
+});
