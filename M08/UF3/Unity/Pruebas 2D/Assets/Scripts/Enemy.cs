96bae74890d55b15033a1e542c80c563d6a71e6a
a18marcastru <a18marcastru@inspedralbes.cat>
2025-02-06 12:21:33 +0100

    Implementacion de NavMesh

---
+++ M08/UF3/Unity/Pruebas 2D/Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    UnityEngine.AI.NavMeshAgent agent;
+
+    private void Start() {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    private void Update() {
+        agent.SetDestination(target.position);
+    }
+}
